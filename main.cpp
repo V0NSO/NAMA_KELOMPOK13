a74af407ee321f45d886de61de40f8d08117599f
V0NSO <102427335+V0NSO@users.noreply.github.com>
2022-03-27 11:14:05 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string arr[2][3] = {{"eli","vonso","Lucky"},{"bersepeda","tidur","makan"}};
+
+    //array [2][3] sebab ada 2 baris d an 3 kolom
+    cout<<" ===peluar beberapa aktivitas yang dilakukan 3 bersaudara===="<<endl;
+    cout<<arr[0][0]<< " sedang "<<arr[1][0]<<endl;
+    cout<<arr[0][1]<< " sedang "<< arr[1][1]<<endl;
+    cout<<arr[0][2]<<" sedang "<<arr[1][2]<<endl;
+    return 0;
+
+
+
+
+
+}
+
+
+int main(){
+char huruf[5] = {'a','b','c','d','e'};
+
+//mengembalikan data pada array
+cout<<"huruf : "<< huruf[2] <<endl;
+return 0;
+
+}
+
+
+int main(){
+int volume,panjang,lebar,tinggi;
+panjang = 20;
+lebar = 30;
+tinggi = 40;
+volume = panjang*lebar*tinggi;
+cout<<"\n Panjang dari volume diatas adalahhhhh\n"<<endl;
+
+cout<<"\n volume diatas = "<<volume<<endl;
+return 0;
+
+
+
+
+
+
+
+
+
+
+
+
